c3b8925db606d2ac2e9e1928daf60450b205911f
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-03-29 01:17:04 +0530

    adding tthe extra data in popular and tweet the grid in the popular component

--- components/Popular/Popular.jsx
+++ components/Popular/Popular.jsx
@@ -15,7 +15,7 @@ const Popular = () => {
   
       {/* Centered Grid Container */}
       <div className="flex justify-center">
-        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4 sm:gap-6 w-full max-w-6xl ">
+        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 w-full max-w-6xl ">
           {data_product.map((item) => (
             <Item
               key={item.id}
--- components/assets/data.js
+++ components/assets/data.js
@@ -2,6 +2,12 @@ import p1_img from './product_1.png'
 import p2_img from './product_2.png'
 import p3_img from './product_3.png'
 import p4_img from './product_4.png'
+import p5_img from './product_5.png'
+import p6_img from './product_6.png'
+import p7_img from './product_7.png'
+import p8_img from './product_8.png'
+
+
 
 let data_product = [
   {
@@ -29,6 +35,35 @@ let data_product = [
     new_price:100.00,
     old_price:150.00,
   },
+  {
+    id: 5,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p5_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+   {
+      id: 6,
+      name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+      image: p6_img,
+      new_price: 85.0,
+      old_price: 120.5,
+    },
+    {
+        id: 7,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p7_img,
+        new_price: 85.0,
+        old_price: 120.5,
+      },
+       {
+          id: 8,
+          name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+          image: p8_img,
+          new_price: 85.0,
+          old_price: 120.5,
+        },
+  
 ];
 
 export default data_product;
